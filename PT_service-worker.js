d62d48d7b75368a5ce65ba81fbb6640a1cff3805
BFSfortecjb <fortecjb@bfs-prevention.fr>
2026-09-21 15:51:27 +0200

    Add files via upload

--- PT_service-worker.js
+++ PT_service-worker.js
@@ -7,7 +7,7 @@
 // données (/auth/, /rest/).
 // =========================================================================
 
-const VERSION_CACHE = 'pointage-v38';
+const VERSION_CACHE = 'pointage-v39';
 
 const FICHIERS_APP_SHELL = [
   './',
